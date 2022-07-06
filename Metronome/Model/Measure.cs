70f718e1720e96be15ecb815c900ae3c4caa41e9
BelyaevAndrey <andrew205103@gmail.com>
2022-07-06 10:39:57 +0300

    Сделал первичную ViewModel

---
+++ Metronome/Model/Measure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metronome.Model;
+
+public static class MeasureInfo
+{
+    public static string MeasureToString(Measure measure)
+    {
+        return measure switch
+        {
+            Measure.Two => "2",
+            Measure.Three => "3",
+            Measure.Four => "4",
+            Measure.Five => "5",
+            Measure.ThreePlusTwo => "3+2",
+            Measure.TwoPlusThree => "2+3",
+            Measure.Six => "6",
+            Measure.Seven => "7",
+            Measure.ThreePlusFour => "3+4",
+            Measure.FourPlusThree => "4+3",
+            Measure.Eight => "8",
+            _ => throw new ArgumentOutOfRangeException(nameof(measure), measure, null)
+        };
+    }
+}
+public enum Measure
+{
+    Two,
+    Three,
+    Four,
+    Five,
+    ThreePlusTwo,
+    TwoPlusThree,
+    Six,
+    Seven,
+    ThreePlusFour,
+    FourPlusThree,
+    Eight
+}
+
---
+++ Metronome/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Metronome.Model;
+
+namespace Metronome.ViewModel;
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    public MainWindowViewModel()
+    {
+        Bpm = 90;
+        CurrentMeasure = 2;
+        MeasuresList = new List<int>();
+        for (int i = 2; i <= 8; i++)
+        {
+            MeasuresList.Add(i);
+        }
+
+        CurrentFaze = 0;
+
+    }
+    private int _bpm;
+
+    public int Bpm
+    {
+        get => _bpm;
+        set
+        {
+            if (value < 1)
+                _bpm = 1;
+            else if (value > 300)
+                _bpm = 300;
+            else
+                _bpm = value;
+            OnPropertyChanged("Bpm");
+        }
+    }
+
+    private int _currentFaze;
+
+    public int CurrentFaze
+    {
+        get => _currentFaze;
+        internal set
+        {
+            _currentFaze = value;
+            OnPropertyChanged("CurrentFaze");
+        }
+    }
+
+    private int _currentMeasure;
+
+    public int CurrentMeasure
+    {
+        get => _currentMeasure;
+        set
+        {
+            _currentMeasure = value;
+            OnPropertyChanged("CurrentMeasure");
+        }
+    }
+
+    public List<int> MeasuresList { get; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
