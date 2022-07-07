3788ac85877c1965d2a307c56168919a78fea14b
BelyaevAndrey <andrew205103@gmail.com>
2022-07-07 09:39:15 +0300

    Первая версия работы метронома

---
+++ Metronome/Model/Metronome.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Metronome.Annotations;
+
+namespace Metronome.Model;
+public class Metronome : INotifyPropertyChanged
+{
+    public Metronome(int bpm, int currentMeasure)
+    {
+        this.Bpm = bpm;
+        this.CurrentMeasure = currentMeasure;
+        this.CurrentFaze = 0;
+    }
+    private int _bpm;
+
+    public int Bpm
+    {
+        get => _bpm;
+        set
+        {
+            if (value < 1)
+                _bpm = 1;
+            else if (value > 300)
+                _bpm = 300;
+            else
+                _bpm = value;
+            OnPropertyChanged("Bpm");
+        }
+    }
+
+    private int _currentFaze;
+
+    public int CurrentFaze
+    {
+        get => _currentFaze;
+        internal set
+        {
+            _currentFaze = value;
+            OnPropertyChanged("CurrentFaze");
+        }
+    }
+
+    private int _currentMeasure;
+
+    public int CurrentMeasure
+    {
+        get => _currentMeasure;
+        set
+        {
+            _currentMeasure = value;
+            OnPropertyChanged("CurrentMeasure");
+        }
+    }
+
+    public static void MetronomeWorkForTimer(object obj)
+    {
+        Metronome ms = obj as Metronome;
+        ms.MetronomeWork();
+    }
+    public void MetronomeWork()
+    {
+        if (CurrentMeasure == CurrentFaze)
+            CurrentFaze = 1;
+        else
+            CurrentFaze++;
+
+        if (CurrentFaze == 1)
+        {
+            SystemSounds.Beep.Play();
+        }
+        else
+        {
+            SystemSounds.Hand.Play();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- Metronome/ViewModel/MainWindowViewModel.cs
+++ Metronome/ViewModel/MainWindowViewModel.cs
@@ -4,67 +4,42 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Metronome.Model;
+using oLeg_Company_App;
 
 namespace Metronome.ViewModel;
 public class MainWindowViewModel : INotifyPropertyChanged
 {
     public MainWindowViewModel()
     {
-        Bpm = 90;
-        CurrentMeasure = 2;
+        InitCommands();
+        Metronome = new Model.Metronome(90, 2);
         MeasuresList = new List<int>();
         for (int i = 2; i <= 8; i++)
         {
             MeasuresList.Add(i);
         }
-
-        CurrentFaze = 0;
-
     }
-    private int _bpm;
-
-    public int Bpm
-    {
-        get => _bpm;
-        set
-        {
-            if (value < 1)
-                _bpm = 1;
-            else if (value > 300)
-                _bpm = 300;
-            else
-                _bpm = value;
-            OnPropertyChanged("Bpm");
-        }
-    }
-
-    private int _currentFaze;
-
-    public int CurrentFaze
-    {
-        get => _currentFaze;
-        internal set
-        {
-            _currentFaze = value;
-            OnPropertyChanged("CurrentFaze");
-        }
-    }
-
-    private int _currentMeasure;
-
-    public int CurrentMeasure
-    {
-        get => _currentMeasure;
-        set
-        {
-            _currentMeasure = value;
-            OnPropertyChanged("CurrentMeasure");
-        }
-    }
+    
+    public Model.Metronome Metronome { get; set; }  
 
     public List<int> MeasuresList { get; }
+
+    private void InitCommands()
+    {
+        this.StartStopCommand = new DelegateCommand(ExecuteToStartStop);
+    }
+
+    public IDelegateCommand StartStopCommand { protected set; get; }
+
+    void ExecuteToStartStop(object param)
+    {
+        TimerCallback tc = new TimerCallback(Model.Metronome.MetronomeWorkForTimer);
+        Timer timer = new Timer(tc, Metronome, 0, 60000/Metronome.Bpm);
+    }
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
